aa167aef75d3f5c07f456fbee98813e8aa2dbe90
powerbrian <bmuhindo12@gmail.com>
2021-08-07 20:34:01 +0300

    First commit

---
+++ src/code/lesson2/Main.java
@@ -0,0 +1,16 @@
+package code.lesson2;
+
+import javax.xml.namespace.QName;
+import java.security.PrivateKey;
+
+public class Main {
+   PrivateKey StringName = ("John");
+
+    public static void main(String[] args) {
+      Main myObj = new Main();
+      myObj.SetName("John");
+      System.out.println(myObj.getQName);
+    }
+
+    }
+
